701c0190cadb9e9dd82bbf17ef9b73be059c69da
lostzilla <alanaa92@gmail.com>
2022-02-01 22:33:31 -0300

    feat: Wallet route

--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 import TitleStyled from "../../components/title";
 import RegistryStyled from "../../components/registryButton";
 import logOffIcon from "../../assets/logoff.svg"
-import addIcon from "../../assets/plussign.svg"
-import removeIcon from "../../assets/minussign.svg"
+import surplusIcon from "../../assets/plussign.svg"
+import deficitIcon from "../../assets/minussign.svg"
 
 const WalletStyled = styled.div`
 
@@ -27,6 +27,8 @@ align-items: center;
 justify-content: space-between;
 flex-direction: row;
 
+margin-bottom: 22px;
+
 `;
 
 const Footer = styled.footer`
@@ -39,11 +41,45 @@ justify-content: space-between;
 flex-direction: row;
 gap: 15px;
 
+margin-top: 15px;
 `;
+
+const WalletScreen = styled.main`
+width: 100%;
+height: 100%;
+
+background: #FFFFFF;
+border-radius: 5px;
+
+`
+
+const NoRegistry = styled.div`
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+
+  color: #868686;
+`
 
 
 function Wallet() {
 
+  function handleRegistryPost(type) {
+
+    alert(`Clicou para adicionar um ${type} à Wallet`)
+  }
+
+  let userRegistries = []
 
   return (
     <WalletStyled>
@@ -51,14 +87,18 @@ function Wallet() {
         <TitleStyled>Olá, Fulano!</TitleStyled>
         <img src={logOffIcon} alt="Log out from wallet" />
       </Header>
-      <h1>main</h1>
+      <WalletScreen>
+        {userRegistries.length !== 0
+          ? userRegistries.map((el, id) => <h1>`Entry ${id}`</h1>)
+          : <NoRegistry><h1>Não há registros de entrada ou saída</h1></NoRegistry>}
+      </WalletScreen>
       <Footer>
-        <RegistryStyled>
-          <img src={addIcon} alt="Add registry" />
+        <RegistryStyled onClick={() => handleRegistryPost("surplus")} >
+          <img src={surplusIcon} alt="Add surplus registry" />
           <h1>Nova<br />entrada</h1>
         </RegistryStyled>
-        <RegistryStyled>
-          <img src={removeIcon} alt="Delete registry" />
+        <RegistryStyled onClick={() => handleRegistryPost("deficit")}>
+          <img src={deficitIcon} alt="Add deficit registry" />
           <h1>Nova<br />saída</h1>
         </RegistryStyled>
       </Footer>
